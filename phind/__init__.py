ba42c09bbc378aef682ec7d45afe3120e61d80d9
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-04-20 10:23:17 +0200

    Update __init__.py

--- phind/__init__.py
+++ phind/__init__.py
@@ -1,11 +1,64 @@
-from re        import findall
-from json      import loads, dumps
-from queue     import Queue, Empty
-from datetime  import datetime
-from threading import Thread
+from urllib.parse import quote
+from tls_client   import Session
+from time         import time
+from datetime     import datetime
 
-from urllib.parse       import quote
-from curl_cffi.requests import post
+client         = Session(client_identifier='chrome110')
+client.headers = {
+    'authority': 'www.phind.com',
+    'accept': '*/*',
+    'accept-language': 'en,fr-FR;q=0.9,fr;q=0.8,es-ES;q=0.7,es;q=0.6,en-US;q=0.5,am;q=0.4,de;q=0.3',
+    'content-type': 'application/json',
+    'origin': 'https://www.phind.com',
+    'referer': 'https://www.phind.com/search',
+    'sec-ch-ua': '"Chromium";v="110", "Google Chrome";v="110", "Not:A-Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+}
+
+class PhindResponse:
+    
+    class Completion:
+        
+        class Choices:
+            def __init__(self, choice: dict) -> None:
+                self.text           = choice['text']
+                self.content        = self.text.encode()
+                self.index          = choice['index']
+                self.logprobs       = choice['logprobs']
+                self.finish_reason  = choice['finish_reason']
+                
+            def __repr__(self) -> str:
+                return f'''<__main__.APIResponse.Completion.Choices(\n    text           = {self.text.encode()},\n    index          = {self.index},\n    logprobs       = {self.logprobs},\n    finish_reason  = {self.finish_reason})object at 0x1337>'''
+
+        def __init__(self, choices: dict) -> None:
+            self.choices = [self.Choices(choice) for choice in choices]
+
+    class Usage:
+        def __init__(self, usage_dict: dict) -> None:
+            self.prompt_tokens      = usage_dict['prompt_tokens']
+            self.completion_tokens  = usage_dict['completion_tokens']
+            self.total_tokens       = usage_dict['total_tokens']
+
+        def __repr__(self):
+            return f'''<__main__.APIResponse.Usage(\n    prompt_tokens      = {self.prompt_tokens},\n    completion_tokens  = {self.completion_tokens},\n    total_tokens       = {self.total_tokens})object at 0x1337>'''
+    
+    def __init__(self, response_dict: dict) -> None:
+        
+        self.response_dict  = response_dict
+        self.id             = response_dict['id']
+        self.object         = response_dict['object']
+        self.created        = response_dict['created']
+        self.model          = response_dict['model']
+        self.completion     = self.Completion(response_dict['choices'])
+        self.usage          = self.Usage(response_dict['usage'])
+
+    def json(self) -> dict:
+        return self.response_dict
 
 
 class Search:
@@ -17,48 +70,47 @@ class Search:
                     'originalQuery': prompt
                 },
                 'webPages': {
-                    'webSearchUrl' : f'https://www.bing.com/search?q={quote(prompt)}',
-                    'value'        : [],
-                    'totalEstimatedMatches': 0
+                    'webSearchUrl': f'https://www.bing.com/search?q={quote(prompt)}',
+                    'totalEstimatedMatches': 0,
+                    'value': []
                 },
                 'rankingResponse': {
-                    'mainline' : {
+                    'mainline': {
                         'items': []
                     }
                 }
             }
         
-        headers = {
-            'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"macOS"',
-            'authority'    : 'www.phind.com',
-            'origin'       : 'https://www.phind.com',
-            'referer'      : 'https://www.phind.com/search',
-            'user-agent'   : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
-        }
-        
-        return post('https://www.phind.com/api/bing/search', headers=headers, json = { 
+        return client.post('https://www.phind.com/api/bing/search', json = { 
             'q': prompt,
             'userRankList': {},
             'browserLanguage': language}).json()['rawBingResults']
 
-
 class Completion:
-    message_queue    = Queue()
-    stream_completed = False
-    
-    def request(model, prompt, results, creative, detailed, codeContext, language) -> None:
+    def create(
+        model = 'gpt-4', 
+        prompt: str = '', 
+        results: dict = None, 
+        creative: bool = False, 
+        detailed: bool = False, 
+        codeContext: str = '',
+        language: str = 'en') -> PhindResponse:
+        
+        if results is None:
+            results = Search.create(prompt, actualSearch = True)
+        
+        if len(codeContext) > 2999:
+            raise ValueError('codeContext must be less than 3000 characters')
         
         models = {
             'gpt-4' : 'expert',
             'gpt-3.5-turbo' : 'intermediate',
             'gpt-3.5': 'intermediate',
         }
-
+        
         json_data = {
             'question'    : prompt,
-            'bingResults' : results,
+            'bingResults' : results, #response.json()['rawBingResults'],
             'codeContext' : codeContext,
             'options': {
                 'skill'   : models[model],
@@ -69,54 +121,25 @@ class Completion:
             }
         }
         
-        stream_req = post('https://www.phind.com/api/infer/answer', json=json_data, timeout=99999,
-            content_callback = Completion.handle_stream_response,
-            headers = {
-                'authority'    : 'www.phind.com',
-                'origin'       : 'https://www.phind.com',
-                'referer'      : f'https://www.phind.com/search?q={quote(prompt)}&c=&source=searchbox&init=true',
-                'user-agent'   : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+        completion = ''
+        response   = client.post('https://www.phind.com/api/infer/answer', json=json_data, timeout_seconds=200)
+        for line in response.text.split('\r\n\r\n'):
+            completion += (line.replace('data: ', ''))
+            
+        return  PhindResponse({
+            'id'     : f'cmpl-1337-{int(time())}', 
+            'object' : 'text_completion', 
+            'created': int(time()), 
+            'model'  : models[model], 
+            'choices': [{
+                    'text'          : completion, 
+                    'index'         : 0, 
+                    'logprobs'      : None, 
+                    'finish_reason' : 'stop'
+            }], 
+            'usage': {
+                'prompt_tokens'     : len(prompt), 
+                'completion_tokens' : len(completion), 
+                'total_tokens'      : len(prompt) + len(completion)
+            }
         })
-
-        Completion.stream_completed = True
-
-    @staticmethod
-    def create(
-        model       : str = 'gpt-4', 
-        prompt      : str = '', 
-        results     : dict = None, 
-        creative    : bool = False, 
-        detailed    : bool = False, 
-        codeContext : str = '',
-        language    : str = 'en'):
-        
-        if results is None:
-            results = Search.create(prompt, actualSearch = True)
-        
-        if len(codeContext) > 2999:
-            raise ValueError('codeContext must be less than 3000 characters')
-        
-        Thread(target = Completion.request, args=[model, prompt, results, creative, detailed, codeContext, language]).start()
-        
-        while Completion.stream_completed != True or not Completion.message_queue.empty():
-            try:
-                message = Completion.message_queue.get(timeout=0)
-                print(message)
-                for token in findall(r'(?<=data: )(.+?)(?=\r\n\r\n)', message.decode()):
-                    yield token
-                
-            except Empty:
-                pass
-
-    @staticmethod
-    def handle_stream_response(response):
-        Completion.message_queue.put(response)
-
-if __name__ == '__main__':
-    array = ''
-    
-    for message in Completion.create(model='gpt-3.5-turbo', prompt='what is the meaning of life'):
-        print(message)
-        array += message
-        
-    print('\n\n' + array)
