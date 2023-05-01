4ae183f1e7e342d8f1610fc9283350da0ccf56ff
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-05-01 17:20:07 +0200

    Add files via upload

--- OtherAgent/FreeLLM/ChatGPTAPI-unsafe.py
+++ OtherAgent/FreeLLM/ChatGPTAPI-unsafe.py
@@ -1,6 +1,5 @@
-from .pyChatGPT import ChatGPT as Chatbot
-#from pyChatGPT import ChatGPT as Chatbot
 
+from revChatGPT.V1 import Chatbot
 import requests
 from langchain.llms.base import LLM
 from typing import Optional, List, Mapping, Any
@@ -18,6 +17,7 @@ class ChatGPT(LLM):
     chatbot : Optional[Chatbot] = None
     call : int = 0
     conversation : Optional[str] = ""
+    prev_message : str = ""
     
     #### WARNING : for each api call this library will create a new chat on chat.openai.com
     
@@ -31,16 +31,17 @@ class ChatGPT(LLM):
             pass
             #raise ValueError("stop kwargs are not permitted.")
         #token is a must check
-        if self.chatbot is None:
-            if self.token is None:
-                raise ValueError("Need a token , check https://chat.openai.com/api/auth/session for get your token")
+        if self.token is None:
+            raise ValueError("Need a token , check https://chat.openai.com/api/auth/session for get your token")
+        else:
+            if self.conversation == "":
+                self.chatbot = Chatbot(config={"access_token": self.token})
+            elif self.conversation != "" and self.prev_message == "":
+                self.chatbot = Chatbot(config={"access_token": self.token}, conversation_id=self.conversation)
+            elif self.conversation != "" and self.prev_message != "":
+                    self.chatbot = Chatbot(config={"access_token": self.token}, conversation_id=self.conversation, parent_id=self.prev_message)
             else:
-                if self.conversation == "":
-                    self.chatbot = Chatbot(self.token)
-                elif self.conversation != "" :
-                    self.chatbot = Chatbot(self.token, conversation_id=self.conversation)
-                else:
-                    raise ValueError("Something went wrong")
+                raise ValueError("Something went wrong")
             
         response = ""
         # OpenAI: 50 requests / hour for each account
@@ -48,12 +49,12 @@ class ChatGPT(LLM):
             raise ValueError("You have reached the maximum number of requests per hour ! Help me to Improve. Abusing this tool is at your own risk")
         else:
             sleep(2)
-            data = self.chatbot.send_message(prompt)
-            #print(data)
-            response = data["message"]
-            self.conversation = data["conversation_id"]
-            FullResponse = data
+            for data in self.chatbot.ask( prompt , conversation_id=self.conversation, parent_id=self.prev_message):
+                response = data["message"]
+                FullResponse = data
             
+            self.conversation = FullResponse["conversation_id"]
+            self.prev_message = FullResponse["parent_id"]
             self.call += 1
         
         #add to history
@@ -67,10 +68,8 @@ class ChatGPT(LLM):
         return {"model": "ChatGPT", "token": self.token}
 
 
-
-#llm = ChatGPT(token = "YOUR-COOKIE") #for start new chat
-
-#llm = ChatGPT(token = "YOUR-COOKIE", conversation = "Add-XXXX-XXXX-Convesation-ID") #for use a chat already started
+#llm = ChatGPT(token = "YOUR_TOKEN") #for start new chat
+#llm = ChatGPT(token = "YOUR-TOKEN", conversation = "Add-XXXX-XXXX-Convesation-ID") #for use a chat already started
 
 #print(llm("Hello, how are you?"))
 #print(llm("what is AI?"))
--- OtherAgent/csvAgent.py
+++ OtherAgent/csvAgent.py
@@ -4,19 +4,31 @@ from langchain.utilities import PythonREPL
 import os
 
 #### LOG IN FOR CHATGPT FREE LLM
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("--chatgpt_token", help="chatgpt token, check https://chat.openai.com/api/auth/session for get your token")
-args = parser.parse_args()
+try :
+    #read from args the hf token and chatgpt token 
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--chatgpt_token", help="Chatgpt token : \n \
+    Go to https://chat.openai.com/chat and open the developer tools by F12. \n \
+    Find the __Secure-next-auth.session-token cookie in Application > Storage > Cookies > https://chat.openai.com \n \
+    Copy the value in the Cookie Value field.")
+    args = parser.parse_args()
 
-if args.chatgpt_token is None:
-    raise Exception("You must provide the huggingface token and chatgpt token")
+    if args.chatgpt_token is None:
+        raise Exception("You must provide the huggingface token and chatgpt token")
 
-os.environ["CHATGPT_TOKEN"] = args.chatgpt_token
+    os.environ["CHATGPT_TOKEN"] = args.chatgpt_token
+except:
+    print("You must provide the chatgpt token")
+    print("chatgpt token: \n \
+    Go to https://chat.openai.com/chat and open the developer tools by F12. \n \
+    Find the __Secure-next-auth.session-token cookie in Application > Storage > Cookies > https://chat.openai.com \n \
+    Copy the value in the Cookie Value field.")
+    CG_TOKEN = input("Insert chatgpt token >>> ")
+    os.environ["CHATGPT_TOKEN"] = CG_TOKEN
 
 llm = ChatGPTAPI.ChatGPT(token=os.environ["CHATGPT_TOKEN"])
 ####
-
 
 path_csv = input("Enter the path of the csv file: ")
 
