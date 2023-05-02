f40899394fbc74270bdd79589be7ce588e08d133
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-05-02 09:29:48 +0200

    fix main version in  uc.Chrome()

--- FreeLLM/pyChatGPT.py
+++ FreeLLM/pyChatGPT.py
@@ -186,7 +186,7 @@ class ChatGPT:
         for arg in self.__chrome_args:
             options.add_argument(arg)
         try:
-            self.driver = uc.Chrome(options=options)
+            self.driver = uc.Chrome(options=options, version_main=112)
         except TypeError as e:
             if str(e) == 'expected str, bytes or os.PathLike object, not NoneType':
                 raise ValueError('Chrome installation not found')
