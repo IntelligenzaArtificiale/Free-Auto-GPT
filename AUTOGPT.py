ce810d3e5373edff19e6e4e12d09833b6e56cd7a
Jaydin_MacBook <JaydinFreemanWork@gmail.com>
2023-05-11 10:43:27 -0400

    Formatting & Warning Removal
    Formatted all Models
    
    Added optional line to remove warning in AUTOgpt

--- AUTOGPT.py
+++ AUTOGPT.py
@@ -1,7 +1,7 @@
 # !pip install bs4
 # !pip install nest_asyncio
 
-# General 
+# General
 import os
 import json
 import pandas as pd
@@ -9,10 +9,10 @@ from dotenv import load_dotenv
 from pathlib import Path
 from json import JSONDecodeError
 from langchain.experimental.autonomous_agents.autogpt.agent import AutoGPT
-from FreeLLM import ChatGPTAPI # FREE CHATGPT API 
-from FreeLLM import HuggingChatAPI # FREE HUGGINGCHAT API
-from FreeLLM import BingChatAPI # FREE BINGCHAT API
-from FreeLLM import BardChatAPI # FREE GOOGLE BARD API
+from FreeLLM import ChatGPTAPI  # FREE CHATGPT API
+from FreeLLM import HuggingChatAPI  # FREE HUGGINGCHAT API
+from FreeLLM import BingChatAPI  # FREE BINGCHAT API
+from FreeLLM import BardChatAPI  # FREE GOOGLE BARD API
 from langchain.agents.agent_toolkits.pandas.base import create_pandas_dataframe_agent
 from langchain.docstore.document import Document
 import asyncio
@@ -21,69 +21,86 @@ import nest_asyncio
 
 # Needed synce jupyter runs an async eventloop
 nest_asyncio.apply()
+# [Optional] Set the environment variable Tokenizers_PARALLELISM to false to get rid of the warning
+# os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 load_dotenv()
-select_model = input("Select the model you want to use (1, 2, 3 or 4) \n \
+select_model = input(
+    "Select the model you want to use (1, 2, 3 or 4) \n \
 1) ChatGPT \n \
 2) HuggingChat \n \
 3) BingChat \n \
 4) Google Bard \n \
->>> ")
+>>> "
+)
 
 if select_model == "1":
     CG_TOKEN = os.getenv("CHATGPT_TOKEN", "your-chatgpt-token")
 
-    if (CG_TOKEN != "your-chatgpt-token"):
+    if CG_TOKEN != "your-chatgpt-token":
         os.environ["CHATGPT_TOKEN"] = CG_TOKEN
     else:
-        raise ValueError("ChatGPT Token EMPTY. Edit the .env file and put your ChatGPT token")
+        raise ValueError(
+            "ChatGPT Token EMPTY. Edit the .env file and put your ChatGPT token"
+        )
 
     start_chat = os.getenv("USE_EXISTING_CHAT", False)
     if os.getenv("USE_GPT4") == "True":
         model = "gpt4"
     else:
         model = "default"
-        
+
     if start_chat:
         chat_id = os.getenv("CHAT_ID")
         if chat_id == None:
             raise ValueError("You have to set up your chat-id in the .env file")
-        llm= ChatGPTAPI.ChatGPT(token=os.environ["CHATGPT_TOKEN"], conversation=chat_id , model=model)
+        llm = ChatGPTAPI.ChatGPT(
+            token=os.environ["CHATGPT_TOKEN"], conversation=chat_id, model=model
+        )
     else:
-        llm= ChatGPTAPI.ChatGPT(token=os.environ["CHATGPT_TOKEN"], model=model)
-              
+        llm = ChatGPTAPI.ChatGPT(token=os.environ["CHATGPT_TOKEN"], model=model)
+
 elif select_model == "2":
-    llm=HuggingChatAPI.HuggingChat()
+    llm = HuggingChatAPI.HuggingChat()
 
 elif select_model == "3":
     if not os.path.exists("cookiesBing.json"):
-        raise ValueError("File 'cookiesBing.json' not found! Create it and put your cookies in there in the JSON format.")
+        raise ValueError(
+            "File 'cookiesBing.json' not found! Create it and put your cookies in there in the JSON format."
+        )
     cookie_path = Path() / "cookiesBing.json"
-    with open("cookiesBing.json", 'r') as file:
+    with open("cookiesBing.json", "r") as file:
         try:
             file_json = json.loads(file.read())
         except JSONDecodeError:
-            raise ValueError("You did not put your cookies inside 'cookiesBing.json'! You can find the simple guide to get the cookie file here: https://github.com/acheong08/EdgeGPT/tree/master#getting-authentication-required.")
-    llm=BingChatAPI.BingChat(cookiepath=str(cookie_path), conversation_style="creative")
+            raise ValueError(
+                "You did not put your cookies inside 'cookiesBing.json'! You can find the simple guide to get the cookie file here: https://github.com/acheong08/EdgeGPT/tree/master#getting-authentication-required."
+            )
+    llm = BingChatAPI.BingChat(
+        cookiepath=str(cookie_path), conversation_style="creative"
+    )
 
 elif select_model == "4":
     GB_TOKEN = os.getenv("BARDCHAT_TOKEN", "your-googlebard-token")
-    
+
     if GB_TOKEN != "your-googlebard-token":
         os.environ["BARDCHAT_TOKEN"] = GB_TOKEN
     else:
-        raise ValueError("GoogleBard Token EMPTY. Edit the .env file and put your GoogleBard token")
+        raise ValueError(
+            "GoogleBard Token EMPTY. Edit the .env file and put your GoogleBard token"
+        )
     cookie_path = os.environ["BARDCHAT_TOKEN"]
-    llm=BardChatAPI.BardChat(cookie=cookie_path)
-    
+    llm = BardChatAPI.BardChat(cookie=cookie_path)
 
 
 HF_TOKEN = os.getenv("HUGGINGFACE_TOKEN", "your-huggingface-token")
 
-if (HF_TOKEN != "your-huggingface-token"):
+if HF_TOKEN != "your-huggingface-token":
     os.environ["HUGGINGFACEHUB_API_TOKEN"] = HF_TOKEN
 else:
-    raise ValueError("HuggingFace Token EMPTY. Edit the .env file and put your HuggingFace token")
+    raise ValueError(
+        "HuggingFace Token EMPTY. Edit the .env file and put your HuggingFace token"
+    )
 
 # Tools
 import os
@@ -95,6 +112,8 @@ from langchain.tools.file_management.write import WriteFileTool
 from tempfile import TemporaryDirectory
 
 ROOT_DIR = TemporaryDirectory()
+
+
 @contextmanager
 def pushd(new_dir):
     """Context manager for changing the current working directory."""
@@ -104,6 +123,7 @@ def pushd(new_dir):
         yield
     finally:
         os.chdir(prev_dir)
+
 
 @tool
 def process_csv(
@@ -126,17 +146,20 @@ def process_csv(
             return result
         except Exception as e:
             return f"Error: {e}"
-        
-        
+
+
 # !pip install playwright
 # !playwright install
 async def async_load_playwright(url: str) -> str:
     """Load the specified URLs using Playwright and parse using BeautifulSoup."""
     from bs4 import BeautifulSoup
     from playwright.async_api import async_playwright
+
     try:
         print(">>> WARNING <<<")
-        print("If you are running this for the first time, you nedd to install playwright")
+        print(
+            "If you are running this for the first time, you nedd to install playwright"
+        )
         print(">>> AUTO INSTALLING PLAYWRIGHT <<<")
         os.system("playwright install")
         print(">>> PLAYWRIGHT INSTALLED <<<")
@@ -165,9 +188,11 @@ async def async_load_playwright(url: str) -> str:
         await browser.close()
     return results
 
+
 def run_async(coro):
     event_loop = asyncio.get_event_loop()
     return event_loop.run_until_complete(coro)
+
 
 @tool
 def browse_web_page(url: str) -> str:
@@ -179,23 +204,31 @@ from langchain.tools import BaseTool, DuckDuckGoSearchRun
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 from pydantic import Field
-from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain, BaseCombineDocumentsChain
+from langchain.chains.qa_with_sources.loading import (
+    load_qa_with_sources_chain,
+    BaseCombineDocumentsChain,
+)
+
 
 def _get_text_splitter():
     return RecursiveCharacterTextSplitter(
         # Set a really small chunk size, just to show.
-        chunk_size = 500,
-        chunk_overlap  = 20,
-        length_function = len,
+        chunk_size=500,
+        chunk_overlap=20,
+        length_function=len,
     )
 
 
 class WebpageQATool(BaseTool):
     name = "query_webpage"
-    description = "Browse a webpage and retrieve the information relevant to the question."
-    text_splitter: RecursiveCharacterTextSplitter = Field(default_factory=_get_text_splitter)
+    description = (
+        "Browse a webpage and retrieve the information relevant to the question."
+    )
+    text_splitter: RecursiveCharacterTextSplitter = Field(
+        default_factory=_get_text_splitter
+    )
     qa_chain: BaseCombineDocumentsChain
-    
+
     def _run(self, url: str, question: str) -> str:
         """Useful for browsing websites and scraping the text information."""
         result = browse_web_page.run(url)
@@ -204,15 +237,23 @@ class WebpageQATool(BaseTool):
         results = []
         # TODO: Handle this with a MapReduceChain
         for i in range(0, len(web_docs), 4):
-            input_docs = web_docs[i:i+4]
-            window_result = self.qa_chain({"input_documents": input_docs, "question": question}, return_only_outputs=True)
+            input_docs = web_docs[i : i + 4]
+            window_result = self.qa_chain(
+                {"input_documents": input_docs, "question": question},
+                return_only_outputs=True,
+            )
             results.append(f"Response from window {i} - {window_result}")
-        results_docs = [Document(page_content="\n".join(results), metadata={"source": url})]
-        return self.qa_chain({"input_documents": results_docs, "question": question}, return_only_outputs=True)
-    
+        results_docs = [
+            Document(page_content="\n".join(results), metadata={"source": url})
+        ]
+        return self.qa_chain(
+            {"input_documents": results_docs, "question": question},
+            return_only_outputs=True,
+        )
+
     async def _arun(self, url: str, question: str) -> str:
         raise NotImplementedError
-      
+
 
 query_website_tool = WebpageQATool(qa_chain=load_qa_with_sources_chain(llm))
 
@@ -221,13 +262,13 @@ query_website_tool = WebpageQATool(qa_chain=load_qa_with_sources_chain(llm))
 import faiss
 from langchain.vectorstores import FAISS
 from langchain.docstore import InMemoryDocstore
-from Embedding import HuggingFaceEmbedding # EMBEDDING FUNCTION
+from Embedding import HuggingFaceEmbedding  # EMBEDDING FUNCTION
 
 from langchain.tools.human.tool import HumanInputRun
 
 # Define your embedding model
 embeddings_model = HuggingFaceEmbedding.newEmbeddingFunction
-embedding_size = 1536 # if you change this you need to change also in Embedding/HuggingFaceEmbedding.py
+embedding_size = 1536  # if you change this you need to change also in Embedding/HuggingFaceEmbedding.py
 index = faiss.IndexFlatL2(embedding_size)
 vectorstore = FAISS(embeddings_model, index, InMemoryDocstore({}), {})
 
--- BABYAGI.py
+++ BABYAGI.py
@@ -10,10 +10,10 @@ from langchain import HuggingFaceHub
 from langchain.docstore import InMemoryDocstore
 from langchain import LLMChain, PromptTemplate
 from langchain.llms import BaseLLM
-from FreeLLM import HuggingChatAPI # FREE HUGGINGCHAT API
-from FreeLLM import ChatGPTAPI # FREE CHATGPT API 
-from FreeLLM import BingChatAPI # FREE BINGCHAT API
-from FreeLLM import BardChatAPI # FREE GOOGLE BARD API
+from FreeLLM import HuggingChatAPI  # FREE HUGGINGCHAT API
+from FreeLLM import ChatGPTAPI  # FREE CHATGPT API
+from FreeLLM import BingChatAPI  # FREE BINGCHAT API
+from FreeLLM import BardChatAPI  # FREE GOOGLE BARD API
 from langchain.vectorstores.base import VectorStore
 from pydantic import BaseModel, Field
 from langchain.chains.base import Chain
@@ -24,71 +24,85 @@ import faiss
 
 load_dotenv()
 
-select_model = input("Select the model you want to use (1, 2, 3 or 4) \n \
+select_model = input(
+    "Select the model you want to use (1, 2, 3 or 4) \n \
 1) ChatGPT \n \
 2) HuggingChat \n \
 3) BingChat (NOT GOOD RESULT)\n \
 4) BardChat \n \
->>> ")
+>>> "
+)
 
 if select_model == "1":
     CG_TOKEN = os.getenv("CHATGPT_TOKEN", "your-chatgpt-token")
 
-    if (CG_TOKEN != "your-chatgpt-token"):
+    if CG_TOKEN != "your-chatgpt-token":
         os.environ["CHATGPT_TOKEN"] = CG_TOKEN
     else:
-        raise ValueError("ChatGPT Token EMPTY. Edit the .env file and put your ChatGPT token")
+        raise ValueError(
+            "ChatGPT Token EMPTY. Edit the .env file and put your ChatGPT token"
+        )
 
     start_chat = os.getenv("USE_EXISTING_CHAT", False)
     if os.getenv("USE_GPT4") == "True":
         model = "gpt4"
     else:
         model = "default"
-        
+
     if start_chat:
         chat_id = os.getenv("CHAT_ID")
         if chat_id == None:
             raise ValueError("You have to set up your chat-id in the .env file")
-        llm= ChatGPTAPI.ChatGPT(token=os.environ["CHATGPT_TOKEN"], conversation=chat_id , model=model)
+        llm = ChatGPTAPI.ChatGPT(
+            token=os.environ["CHATGPT_TOKEN"], conversation=chat_id, model=model
+        )
     else:
-        llm= ChatGPTAPI.ChatGPT(token=os.environ["CHATGPT_TOKEN"], model=model)
-              
+        llm = ChatGPTAPI.ChatGPT(token=os.environ["CHATGPT_TOKEN"], model=model)
+
 elif select_model == "2":
-    llm=HuggingChatAPI.HuggingChat()
+    llm = HuggingChatAPI.HuggingChat()
 
 elif select_model == "3":
     if not os.path.exists("cookiesBing.json"):
-        raise ValueError("File 'cookiesBing.json' not found! Create it and put your cookies in there in the JSON format.")
+        raise ValueError(
+            "File 'cookiesBing.json' not found! Create it and put your cookies in there in the JSON format."
+        )
     cookie_path = Path() / "cookiesBing.json"
-    with open("cookiesBing.json", 'r') as file:
+    with open("cookiesBing.json", "r") as file:
         try:
             file_json = json.loads(file.read())
         except JSONDecodeError:
-            raise ValueError("You did not put your cookies inside 'cookiesBing.json'! You can find the simple guide to get the cookie file here: https://github.com/acheong08/EdgeGPT/tree/master#getting-authentication-required.")
-    llm=BingChatAPI.BingChat(cookiepath=str(cookie_path), conversation_style="creative")
+            raise ValueError(
+                "You did not put your cookies inside 'cookiesBing.json'! You can find the simple guide to get the cookie file here: https://github.com/acheong08/EdgeGPT/tree/master#getting-authentication-required."
+            )
+    llm = BingChatAPI.BingChat(
+        cookiepath=str(cookie_path), conversation_style="creative"
+    )
 
 elif select_model == "4":
     GB_TOKEN = os.getenv("BARDCHAT_TOKEN", "your-googlebard-token")
-    
+
     if GB_TOKEN != "your-googlebard-token":
         os.environ["BARDCHAT_TOKEN"] = GB_TOKEN
     else:
-        raise ValueError("GoogleBard Token EMPTY. Edit the .env file and put your GoogleBard token")
+        raise ValueError(
+            "GoogleBard Token EMPTY. Edit the .env file and put your GoogleBard token"
+        )
     cookie_path = os.environ["BARDCHAT_TOKEN"]
-    llm=BardChatAPI.BardChat(cookie=cookie_path)
-    
-    
+    llm = BardChatAPI.BardChat(cookie=cookie_path)
+
 
 HF_TOKEN = os.getenv("HUGGINGFACE_TOKEN", "your-huggingface-token")
 
-if (HF_TOKEN != "your-huggingface-token"):
+if HF_TOKEN != "your-huggingface-token":
     os.environ["HUGGINGFACEHUB_API_TOKEN"] = HF_TOKEN
 else:
-    raise ValueError("HuggingFace Token EMPTY. Edit the .env file and put your HuggingFace token")
+    raise ValueError(
+        "HuggingFace Token EMPTY. Edit the .env file and put your HuggingFace token"
+    )
 
 
-
-from Embedding import HuggingFaceEmbedding # EMBEDDING FUNCTION
+from Embedding import HuggingFaceEmbedding  # EMBEDDING FUNCTION
 
 # Define your embedding model
 embeddings_model = HuggingFaceEmbedding.newEmbeddingFunction
@@ -99,7 +113,7 @@ vectorstore = FAISS(embeddings_model, index, InMemoryDocstore({}), {})
 
 print(vectorstore)
 
-#DEFINE TOOL
+# DEFINE TOOL
 from langchain.agents import ZeroShotAgent, Tool, AgentExecutor
 from langchain import OpenAI, LLMChain
 from langchain.tools import BaseTool, DuckDuckGoSearchRun
@@ -146,19 +160,26 @@ agent_executor = AgentExecutor.from_agent_and_tools(
 # Logging of LLMChains
 verbose = False
 
-int_max_iterations = input("Enter the maximum number of iterations: (Suggest from 3 and 5) ")
+int_max_iterations = input(
+    "Enter the maximum number of iterations: (Suggest from 3 and 5) "
+)
 max_iterations = int(int_max_iterations)
 
-if input("Do you want to store the results? (y/n) ") == "y" :
+if input("Do you want to store the results? (y/n) ") == "y":
     store_results = True
 else:
     store_results = False
 
 
 # If None, will keep on going forever
-max_iterations: Optional[int] =   max_iterations
+max_iterations: Optional[int] = max_iterations
 baby_agi = BabyAGIMod.BabyAGI.from_llm(
-    llm=llm, vectorstore=vectorstore, task_execution_chain=agent_executor, verbose=verbose, max_iterations=max_iterations, store= store_results
+    llm=llm,
+    vectorstore=vectorstore,
+    task_execution_chain=agent_executor,
+    verbose=verbose,
+    max_iterations=max_iterations,
+    store=store_results,
 )
 
 
