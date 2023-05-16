7be6ebca37b827644928bb9de9bf82a5c55975b3
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-05-16 10:32:42 +0200

    update and fix

--- OtherAgent/pythonAgent.py
+++ OtherAgent/pythonAgent.py
@@ -44,7 +44,19 @@ if select_model == "1":
         llm= ChatGPTAPI.ChatGPT(token=os.environ["CHATGPT_TOKEN"], model=model)
               
 elif select_model == "2":
-    llm=HuggingChatAPI.HuggingChat()
+    if not os.path.exists("cookiesHuggingChat.json"):
+        raise ValueError(
+            "File 'cookiesHuggingChat.json' not found! Create it and put your cookies in there in the JSON format."
+        )
+    cookie_path = Path() / "cookiesHuggingChat.json"
+    with open("cookiesHuggingChat.json", "r") as file:
+        try:
+            file_json = json.loads(file.read())
+        except JSONDecodeError:
+            raise ValueError(
+                "You did not put your cookies inside 'cookiesHuggingChat.json'! You can find the simple guide to get the cookie file here: https://github.com/IntelligenzaArtificiale/Free-Auto-GPT"
+            )  
+    llm = HuggingChatAPI.HuggingChat(cookiepath = str(cookie_path))
 
 elif select_model == "3":
     if not os.path.exists("cookiesBing.json"):
